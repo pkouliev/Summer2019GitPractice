b336e09dea2c9800242de1400923d302bf88c340
Parvin Kouliev <57721678+pkouliev@users.noreply.github.com>
2020-12-24 22:45:50 -0500

    Update Person.java

--- src/main/java/java_code/Person.java
+++ src/main/java/java_code/Person.java
@@ -2,9 +2,7 @@ package java_code;
 
 public class Person {
     private int age;
-    private String firstName;
-    private String address;
-
+    
     public Person(int age) {
         this.age = age;
     }
